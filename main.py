4255b83fc4f11c8c7c62ddd991da20912e15097a
b10t <b10t.rus@gmail.com>
2021-12-06 16:15:21 +0800

    Создан скелет приложения.

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+
+
+def get_list_programming_languages():
+    """Получить список языков программирования."""
+    pass
+
+
+def get_vacancies_from_hh(language):
+    """Получить вакансии по выбраному языку программирования с hh.ru
+
+    Args:
+        language (str): Язык программирования
+    """
+    pass
+
+
+def show_table_average_salary():
+    """Отобразить таблицу по языкам программирования, со средней зарплатой."""
+    pass
+
+
+def predict_rub_salary():
+    pass
+
+
+if __name__ == "__main__":
+    pass
