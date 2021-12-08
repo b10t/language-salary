e1f4856c9c370b51735d5b3dda6ff66f84016767
b10t <b10t.rus@gmail.com>
2021-12-08 11:02:16 +0800

    Получение данных по вакансиям с HH.

--- main.py
+++ main.py
@@ -1,28 +1,5 @@
-import requests
-
-
-def get_list_programming_languages():
-    """Получить список языков программирования."""
-    pass
-
-
-def get_vacancies_from_hh(language):
-    """Получить вакансии по выбраному языку программирования с hh.ru
-
-    Args:
-        language (str): Язык программирования
-    """
-    pass
-
-
-def show_table_average_salary():
-    """Отобразить таблицу по языкам программирования, со средней зарплатой."""
-    pass
-
-
-def predict_rub_salary():
-    pass
+from fetch_from_headhunter import get_vacancies_from_hh
 
 
 if __name__ == "__main__":
-    pass
+    get_vacancies_from_hh()
