cb51c80772ec116a907201d1626b2adf1868e1ac
b10t <b10t.rus@gmail.com>
2021-12-08 15:35:11 +0800

    Получение вакансий с HH, SJ и их вывод.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from common_functions import show_table_average_salary
 from fetch_from_headhunter import get_vacancies_from_hh
-
+from fetch_from_super_job import get_vacancies_from_sj
 
 if __name__ == "__main__":
     show_table_average_salary('HeadHunter Moscow', get_vacancies_from_hh())
+    show_table_average_salary('SuperJob Moscow', get_vacancies_from_sj())
