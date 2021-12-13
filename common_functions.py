ef660b230144481b72319381d1552e6190d87c63
b10t <b10t.rus@gmail.com>
2021-12-13 10:45:38 +0800

    Высушил код.

--- common_functions.py
+++ common_functions.py
@@ -9,9 +9,7 @@ def predict_salary(salary_from, salary_to):
         return (salary_from + salary_to) / 2
     elif salary_from:
         return salary_from * 1.2
-    elif not salary_from and not salary_to:
-        return None
-    else:
+    elif salary_to:
         return salary_to * 0.8
 
 
